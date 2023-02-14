936453ec5a7cd88cee8e2f40ee71564d075b2686
Zeus Gagarin <vz.gagarin@gmail.com>
2023-02-14 22:51:21 +0800

    v.01

---
+++ band-name-generator.py
@@ -0,0 +1,6 @@
+print("Welcome to band name generator.")
+
+idea1 = input("What is your first idea?:\n")
+idea2 = input("What is your 2nd idea?: \n")
+
+print("Your band name could be: "+ idea1 + " " + idea2)
